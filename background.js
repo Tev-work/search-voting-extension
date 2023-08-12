cc0b873f5e3a147085781068ae1ee9f0a94b4b0c
Tev <petr.macek.prace@gmail.com>
2023-08-12 20:20:46 +0200

    First fully working version

--- background.js
+++ background.js
@@ -1,12 +1,114 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (changeInfo.status === 'complete' && /^https?:\/\/www.google.com\//.test(tab.url)) {
-        console.log("injecting content script on url:", tab.url);
+        chrome.scripting
+            .insertCSS({
+                target: { tabId },
+                files: ["voting-styles.css"],
+            })
         chrome.scripting
             .executeScript({
-                target: { tabId: tabId },
+                target: { tabId },
                 files: ["./search-voting.js"]
             })
-            .then(() => console.log("content script injected"))
-            .catch(err => console.log('injecting content script failed:', err));
+            .catch(err => console.warn('injecting content script failed:', err));
     }
 });
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.getVotes) {
+        (async () => {
+            const votes = await getVotes();
+            sendResponse(votes);
+        })();
+
+        return true;
+    } else if (request['voting-upvote']) {
+        vote(request['voting-upvote'], 'upvote');
+    } else if (request['voting-downvote']) {
+        vote(request['voting-downvote'], 'downvote');
+    }
+});
+
+const TABLE_NAME = "votesByDomain";
+
+
+async function getVotes() {
+    const db = await getDB();
+    return new Promise((resolve, reject) => { 
+        const request = getVotesObjectStore(db).openCursor();
+
+        const allVotes = {};
+        request.onsuccess = (event) => {
+            let cursor = event.target.result;
+            if (cursor) {
+                let key = cursor.primaryKey;
+                let value = cursor.value;
+                allVotes[key] = value;
+                cursor.continue();
+            }
+            else {
+                resolve(allVotes);
+            }
+        }
+
+        request.onerror = (event) => {
+            console.warn('error getting votes', request.errorCode, event);
+            reject();
+        }
+    });
+}
+
+async function vote(hostname, direction) {
+    const db = await getDB();
+
+    return new Promise((resolve, reject) => { 
+        const store = getVotesObjectStore(db, 'readwrite')
+        const request = store.get(hostname);;
+
+        request.onsuccess = (event) => {
+            const diff = direction === 'upvote' ? 1 : -1;
+
+            if (request.result === undefined) {
+                store.add(diff, hostname);
+            } else {
+                store.put(request.result + diff, hostname);
+            }
+            resolve(request.result);
+        }
+
+        request.onerror = (event) => {
+            console.warn('DB error when voting', request.errorCode, event);
+            reject();
+        }
+    });
+}
+
+function getVotesObjectStore(db, mode = 'readonly') {
+    const transaction = db.transaction([TABLE_NAME], mode);
+        
+    transaction.onerror = (event) => {
+        console.warn('transaction error:', error, event);
+    };
+
+    return transaction.objectStore(TABLE_NAME);
+}
+
+async function getDB() {
+    return new Promise((resolve, reject) => { 
+        const dbName = "SearchVotingDatabase";
+        const dbRequest = indexedDB.open(dbName, 3);
+
+        dbRequest.onupgradeneeded = (event) => {
+            const db = event.target.result;
+            db.createObjectStore(TABLE_NAME);
+        };
+
+        dbRequest.onerror = (event) => {
+            console.warn('getting DB error:', dbRequest.errorCode, event);
+            reject();
+        };
+        dbRequest.onsuccess = (/* event */) => {
+            resolve(dbRequest.result);
+        };
+    });
+}
--- search-voting.js
+++ search-voting.js
@@ -1,9 +1,75 @@
-console.log('injected hello search world');
-
-window.searchVotingExtLoaded = false;
 run();
 
-function run () {
-    
-    window.searchVotingExtLoaded = true;
+async function run () {
+    // TODO something more robust
+    const searchResults = document.querySelector('#search > div > div')?.children;
+    if (!searchResults || !searchResults.length) {
+        return;
+    }
+
+    const voteCountElements = [];
+
+    Array.from(searchResults).forEach(element => {
+        const anchor = element.querySelector('a');
+        const url = anchor.href;
+        const hostname = (new URL(url)).hostname.replace('www.', '');
+
+        element.classList.add('search-result');
+
+        const votingContainer = document.createElement('div');
+        votingContainer.className = 'voting-container';
+        
+        const upvoteBtn = document.createElement('button');
+        upvoteBtn.textContent = '+';
+        upvoteBtn.className = 'voting-button voting-upvote';
+        upvoteBtn.votingAction = 'voting-upvote';
+        votingContainer.appendChild(upvoteBtn);
+
+        const voteCount = document.createElement('span');
+        voteCount.textContent = 'LD';
+        voteCount.className = 'voting-count';
+        voteCount.dataset.hostname = hostname;
+        votingContainer.appendChild(voteCount);
+        voteCountElements.push(voteCount);
+        
+        const downvoteBtn = document.createElement('button');
+        downvoteBtn.textContent = '-';
+        downvoteBtn.className = 'voting-button voting-downvote';
+        downvoteBtn.votingAction = 'voting-downvote';
+        votingContainer.appendChild(downvoteBtn);
+
+        const votingListener = (thisBtn, otherBtn) => () => {
+            if (votingContainer.voteSent) {
+                if (thisBtn.voteSent) {
+                    thisBtn.voteSent = false;
+                    votingContainer.voteSent = false;
+                    thisBtn.classList.remove('vote-sent');
+                    chrome.runtime.sendMessage({ [otherBtn.votingAction]: hostname });
+                } else {
+                    otherBtn.voteSent = false;
+                    thisBtn.voteSent = true;
+                    otherBtn.classList.remove('vote-sent');
+                    thisBtn.classList.add('vote-sent');
+                    // sending twice to revert otherBtn vote
+                    chrome.runtime.sendMessage({ [thisBtn.votingAction]: hostname })
+                        .then(() => chrome.runtime.sendMessage({ [thisBtn.votingAction]: hostname }));
+                }
+            } else {
+                thisBtn.voteSent = true;
+                votingContainer.voteSent = true;
+                thisBtn.classList.add('vote-sent');
+                chrome.runtime.sendMessage({ [thisBtn.votingAction]: hostname });
+            }
+        }
+
+        upvoteBtn.addEventListener('click', votingListener(upvoteBtn, downvoteBtn))
+        downvoteBtn.addEventListener('click', votingListener(downvoteBtn, upvoteBtn))
+        
+        element.appendChild(votingContainer);
+    });
+
+    const votes = await chrome.runtime.sendMessage({ getVotes: true });
+    voteCountElements.forEach(element => {
+        element.textContent = votes[element.dataset.hostname] ?? 0;
+    });
 }
